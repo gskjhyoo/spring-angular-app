6fd90f1c9e029c4a6eb4cd4a73474370813f86a1
ktkim <rkdmfql1549@naver.com>
2017-09-21 23:54:18 +0900

    Initial commit

---
+++ src/main/java/kr/ktkim/app/SpringAngularAppApplication.java
@@ -0,0 +1,12 @@
+package kr.ktkim.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAngularAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAngularAppApplication.class, args);
+	}
+}
